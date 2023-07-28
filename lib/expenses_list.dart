0274161176f501257ef3aa2eaa5a0f08208e3efa
Mateusz Mężyk <mateuszmezyk90@gmail.com>
2023-07-28 09:27:27 +0200

    performance optimized list view for expenses. Basic

---
+++ lib/expenses_list.dart
@@ -0,0 +1,20 @@
+import 'package:expense_tracker/model/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    //to build a list fron items that are already visible use builder method. In contrast regular constructor creates full list (although scrollable)
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => Text(expenses[index].title),
+    );
+  }
+}
